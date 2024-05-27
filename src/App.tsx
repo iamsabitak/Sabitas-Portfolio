da540a58b02007eba4b91a7be944f9d47996428e
sabita <sabitakhadka091@gmail.com>
2024-05-27 18:38:17 +0545

    feat: Added Skill component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import About from "./components/About";
 import { HeaderMenu } from "./components/HeaderMenu";
 import Home from "./components/Home";
 import { Fragment } from "react/jsx-runtime";
+import Skills from "./components/Skills";
 
 const App = () => {
   return (
     <Fragment>
       <HeaderMenu />
       <Home />
-      <About/>
+      <About />
+      <Skills />
     </Fragment>
   );
 };
