9d8d79e615cd0b532de74bd9051f920f3d26e2b6
sabita <sabitakhadka091@gmail.com>
2024-05-28 19:04:22 +0545

    refactor: Update The Skill Ui Styling and Design

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,7 +6,6 @@ import {
   Title,
   Text,
   Box,
-  Slider,
 } from "@mantine/core";
 import SkillBgImg from "../assets/Skills.svg";
 import Scroll from "../assets/Scroll.svg";
@@ -24,33 +23,143 @@ const Skills: React.FC = () => {
         <Center>
           <Image src={Scroll} alt="scroll" w={34} h={124} mt={60} mb={60} />
         </Center>
-        <Flex p={"1.5rem 10rem 3rem 10rem"} direction={"column"} pt={70}>
+        <Flex p={"1.5rem 20rem 3rem 20rem"} direction={"column"} pt={70}>
           <Flex justify={"center"} align={"center"} direction={"column"}>
             {" "}
             <Title c={"#12F7D6"} style={{ fontFamily: "monospace" }}>
               Technologies
             </Title>
-            <Text mt={6}>
+            <Text mt={6} fz={18}>
               I am striving to never stop learning and improving
             </Text>
           </Flex>
-          <Box>
-            HTML
-            <Slider
-              defaultValue={60}
-              disabled
-              styles={{ bar: { color: "red" } }}
-            />
-            CSS, Sass, Bootstrap, TailwindCss & Mantine
-            <Slider defaultValue={60} disabled />
-            JavaScript, TypeScript
-            <Slider defaultValue={60} disabled />
-            UI design in Figma
-            <Slider defaultValue={60} disabled />
-            React
-            <Slider defaultValue={60} disabled />
-            NextJs
-            <Slider defaultValue={60} disabled />
+          <Box mt={50}>
+            <Flex justify={"space-between"}>
+              <Text fz={20}> HTML</Text>
+              <Text>Advance</Text>
+            </Flex>
+            <Box
+              mt={8}
+              mb={15}
+              w={"100%"}
+              bg={"grey"}
+              h={17}
+              style={{ borderRadius: "20px" }}
+            >
+              <Box
+                w={"80%"}
+                bg={
+                  "linear-gradient(90deg, rgba(18,247,214,1) 8%, rgba(152,250,236,1) 100%)"
+                }
+                h={17}
+                style={{ borderRadius: "20px" }}
+              ></Box>
+            </Box>
+            <Flex justify={"space-between"}>
+              <Text fz={20}> CSS, Sass, Bootstrap, TailwindCss & Mantine</Text>
+              <Text>Advance</Text>
+            </Flex>
+            <Box
+              mt={8}
+              mb={15}
+              w={"100%"}
+              bg={"grey"}
+              h={17}
+              style={{ borderRadius: "20px" }}
+            >
+              <Box
+                w={"80%"}
+                bg={
+                  "linear-gradient(90deg, rgba(18,247,214,1) 8%, rgba(152,250,236,1) 100%)"
+                }
+                h={17}
+                style={{ borderRadius: "20px" }}
+              ></Box>
+            </Box>
+            <Flex justify={"space-between"}>
+              <Text fz={20}> JavaScript, TypeScript</Text>
+              <Text>Regular</Text>
+            </Flex>{" "}
+            <Box
+              mt={8}
+              mb={15}
+              w={"100%"}
+              bg={"grey"}
+              h={17}
+              style={{ borderRadius: "20px" }}
+            >
+              <Box
+                w={"70%"}
+                bg={
+                  "linear-gradient(90deg, rgba(18,247,214,1) 8%, rgba(152,250,236,1) 100%)"
+                }
+                h={17}
+                style={{ borderRadius: "20px" }}
+              ></Box>
+            </Box>
+            <Flex justify={"space-between"}>
+              <Text fz={20}> UI design in Figma</Text>
+              <Text>Regular</Text>
+            </Flex>
+            <Box
+              mt={8}
+              mb={15}
+              w={"100%"}
+              bg={"grey"}
+              h={17}
+              style={{ borderRadius: "20px" }}
+            >
+              <Box
+                w={"60%"}
+                bg={
+                  "linear-gradient(90deg, rgba(18,247,214,1) 8%, rgba(152,250,236,1) 100%)"
+                }
+                h={17}
+                style={{ borderRadius: "20px" }}
+              ></Box>
+            </Box>
+            <Flex justify={"space-between"}>
+              <Text fz={20}> React</Text>
+              <Text>Regular</Text>
+            </Flex>
+            <Box
+              mt={8}
+              mb={15}
+              w={"100%"}
+              bg={"grey"}
+              h={17}
+              style={{ borderRadius: "20px" }}
+            >
+              <Box
+                w={"70%"}
+                bg={
+                  "linear-gradient(90deg, rgba(18,247,214,1) 8%, rgba(152,250,236,1) 100%)"
+                }
+                h={17}
+                style={{ borderRadius: "20px" }}
+              ></Box>
+            </Box>
+            <Flex justify={"space-between"}>
+              <Text fz={20}> NextJs</Text>
+              <Text>Beginner</Text>
+            </Flex>
+            <Box
+              mt={8}
+              mb={15}
+              w={"100%"}
+              bg={"grey"}
+              h={17}
+              style={{ borderRadius: "20px" }}
+            >
+              <Box
+                w={"40%"}
+                bg={
+                  "linear-gradient(90deg, rgba(18,247,214,1) 8%, rgba(152,250,236,1) 100%)"
+                }
+                h={17}
+                style={{ borderRadius: "20px" }}
+              ></Box>
+            </Box>
           </Box>
         </Flex>
       </BackgroundImage>
