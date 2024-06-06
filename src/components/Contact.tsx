d0fc548498932b056b4f5b7e49b1025a3be735c6
sabita <sabitakhadka091@gmail.com>
2024-06-06 19:17:01 +0545

    feat: created the structure of contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Box, Center, Flex, Image, Text, Title } from "@mantine/core";
 
-const Contact:React.FC = () => {
+import Scroll from "../assets/Scroll.svg";
+import React from "react";
+
+const Contact: React.FC = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <Flex
+      justify={"center"}
+      direction={"column"}
+      c={"#FFFFFF"}
+      style={{ fontFamily: "monospace" }}
+    >
+      {" "}
+      <Box p={"1rem 10rem 3rem 10rem"} bg={"#292F36"}>
+        {" "}
+        <Center>
+          <Image src={Scroll} alt="scroll" w={34} h={124} mt={60} mb={60} />
+        </Center>
+        <Flex justify={"center"} align={"center"} direction={"column"}>
+          {" "}
+          <Title c={"#12F7D6"} style={{ fontFamily: "monospace" }}>
+            Contact
+          </Title>
+          <Text mt={6} fz={18}>
+            I’m currently available for freelance, Omnsite, and remote work
+          </Text>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+};
 
-export default Contact
+export default Contact;
